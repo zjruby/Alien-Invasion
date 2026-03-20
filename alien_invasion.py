8d0395f047f338881a8596df6eb1082de8d94ef3
zoujin <zoujin@zj.com>
2026-03-20 16:11:58 +0800

    更新游戏设计功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -21,12 +22,16 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        #编组
+        self.ship=Ship(self)
+        self.bullets=pygame.sprite.Group()
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()#更新游戏子弹位置
             self._update_screen()
             self.clock.tick(60)
 
@@ -58,6 +63,9 @@ class AlienInvasion:
             self.ship.screen_rect=self.screen.get_rect()
             self.ship.rect.midbottom = self.ship.screen_rect.midbottom
             self.ship.x = float(self.ship.rect.x)
+            #开火
+        elif event.key==pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         # 响应释放
@@ -69,9 +77,15 @@ class AlienInvasion:
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
 
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets"""
+        new_bullet=Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == "__main__":
     # 创建游戏实例并运行游戏
--- bullet.py
+++ bullet.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
  
-class Bulet(Sprite):
+class Bullet(Sprite):
     """管理飞船所发射子弹的类"""
     def __init__(self, ai_game):
         """在飞船的当前位置创建一个子弹对象"""
@@ -11,7 +11,18 @@ class Bulet(Sprite):
         self.color =self.settings.bullet_color
         #在（0,0）处创建一个表示子弹的矩形，在设置正确的位置
         self.rect=pygame.Rect(0,0,self.settings.bullet_width,
-                              self.settings.bullte_height)
+                              self.settings.bullet_height)
         self.rect.midtop=ai_game.ship.rect.midtop
         #存储用浮点数表示的子弹位置
         self.y=float(self.rect.y)
+    
+    def update(self):
+        """向上移动子弹"""
+        #更新子弹的精准位置
+        self.y -=self.settings.bullet_speed
+        #更新表示子弹的rect的位置
+        self.rect.y=self.y
+    
+    def draw_bullet(self):
+        """在屏幕上绘制子弹"""
+        pygame.draw.rect(self.screen,self.color,self.rect)
